2e957b4b728c64c91c310568732dddbb23bdb56b
林楠 <apple@appledeMacBook-Pro.local>
2021-07-11 00:23:24 +0800

    初始化项目；解决云函数调用和路径别名设置问题

---
+++ client/.eslintrc.js
@@ -0,0 +1,47 @@
+module.exports = {
+  "parser": "@typescript-eslint/parser",
+  "env": {
+    "es6": true,
+    "browser": true,
+    "node": true,
+    "commonjs": true,
+  },
+  "parserOptions": {
+    "ecmaVersion": 6,
+    "sourceType": "module",
+    "ecmaFeatures": {
+      "jsx": true,
+      "modules": true
+    },
+    "project": './tsconfig.json',
+  },
+  "extends": [
+    "taro/react",
+    'eslint-config-alloy/react',
+    'eslint-config-alloy/typescript',
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "plugin:taro/all"
+  ],
+  "plugins": [
+    "react",
+    "@typescript-eslint/eslint-plugin",
+  ],
+  "globals": {
+    "window": false,
+    "document": false,
+    "process.env.TARO_ENV": false,
+    "wx": false,
+    "OLDHOST": true
+  },
+  "settings": {
+    "react": {
+      "createClass": "createReactClass", // Regex for Component Factory to use,
+      "pragma": "React",  // Pragma to use, default to "React"
+      "version": "detect", // React version. "detect" automatically picks the version you have installed.
+    },
+  },
+  "rules": {
+    "react/jsx-uses-react": "off",
+    "react/react-in-jsx-scope": "off"
+  }
+}
---
+++ client/src/app.ts
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+import Taro, { Config } from '@tarojs/taro'
+
+import './app.scss'
+
+class App extends Component {
+
+  componentDidMount () {
+    if (process.env.TARO_ENV === 'weapp') {
+      // 调用云函数需要在这里配置对应的环境 ID
+      Taro.cloud.init({
+        env: 'miniprogram-1-0gnl3gry4e088f22'
+      })
+    }
+  }
+
+  componentDidShow () {}
+
+  componentDidHide () {}
+
+  componentDidCatchError () {}
+
+  // this.props.children 是将要会渲染的页面
+  render () {
+    return this.props.children
+  }
+}
+
+export default App
